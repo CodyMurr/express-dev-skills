bb903dfbe430bc1713bde2a2225dad157243888e
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-12 21:16:29 -0600

    Add id property to skills object

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,7 @@
 const skills = [
-    {technology: 'HTML', type: 'Front-End', concepts: ['Semantics', 'Elements/Tags', 'Forms', 'Tables']},
-    {technology: 'CSS', type: 'Front-end', concepts: ['Responsive Design', 'Flexbox', 'Grid', 'Animations', 'Specificity']},
-    {technology: 'Javascript', type: 'Full Stack', concepts: ['DOM Manipulation', 'OOP', 'Dynamic Webpage Rendering']},
+    {id: 561, technology: 'HTML', type: 'Document/Markup', concepts: ['Semantics', 'Elements/Tags', 'Forms', 'Tables']},
+    {id: 860, technology: 'CSS', type: 'Stylesheet', concepts: ['Responsive Design', 'Flexbox', 'Grid', 'Animations', 'Specificity']},
+    {id: 730, technology: 'Javascript', type: 'Object Oriented Scripting', concepts: ['DOM Manipulation', 'OOP', 'Dynamic Webpage Rendering']},
 ];
 
 module.exports = {

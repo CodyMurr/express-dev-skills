8841603e8a1b8313455675f834bda7d57e3b9766
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-14 00:28:55 -0600

    Add create and delete functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,16 +2,33 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
 };
 
-function show(req, res) {
-    const skill = Skill.getOne(req.params.technology);
-    res.render('skills/show', {skill});
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
 }
 
+function create(req, res) {
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+ 
+function newSkill(req, res) {
+    res.render('skills/new');
+}
 function index(req, res) {
     res.render('skills/index', {
         skills: Skill.getAll()
     });
-}
+};
+
+function show(req, res) {
+    res.render('skills/show', {
+        skill: Skill.getOne(req.params.id)
+    });
+};
--- models/skill.js
+++ models/skill.js
@@ -1,15 +1,28 @@
-const skills = [
+let skills = [
     {id: 561, technology: 'HTML', type: 'Document/Markup', concepts: ['Semantics', 'Elements/Tags', 'Forms', 'Tables']},
     {id: 860, technology: 'CSS', type: 'Stylesheet', concepts: ['Responsive Design', 'Flexbox', 'Grid', 'Animations', 'Specificity']},
-    {id: 730, technology: 'Javascript', type: 'Object Oriented Scripting', concepts: ['DOM Manipulation', 'OOP', 'Dynamic Webpage Rendering']},
+    {id: 730, technology: 'Javascript', type: 'Object Oriented Programming', concepts: ['DOM Manipulation', 'Dynamic Webpage Rendering']},
 ];
 
 module.exports = {
-    getAll, 
-    getOne
+    getAll,
+    getOne,
+    create,
+    deleteOne
+};
+
+function deleteOne(id) {
+    id = parseInt(id);
+    skills = skills.filter(skill => skill.id !== id);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000;
+    skills.push(skill);
 }
 
 function getOne(id) {
+    id = parseInt(id);
     return skills.find(skill => skill.id === id);
 }
 
